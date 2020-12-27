727746c0204fe1cee3f4ec1fa95468ec23550a19
Denis Feliciano <denisf.salles@gmail.com>
2020-12-27 15:33:02 -0300

    add catalog items screen

---
+++ src/views/CatalogScreen/CatalogScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import ItemList from '../components/ItemList/ItemList';
+
+const CatalogScreen: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <ItemList />
+    </>
+  );
+};
+
+export default CatalogScreen;
---
+++ src/views/CatalogScreen/styleCatalogScreen.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+const styleCatalog = StyleSheet.create({
+
+});
+
+export default styleCatalog;
